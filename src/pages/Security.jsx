b044e8e0a2059b5e704c5fdd072f440712aa7937
Mohamed Sameh AL-Masri <eng.mohamedtayee@gmail.com>
2025-04-30 10:34:26 +0300

    ..0

--- src/pages/Security.jsx
+++ src/pages/Security.jsx
@@ -5,8 +5,7 @@ import SecurityFeatures from '../components/sections/Security';
 
 const Security = () => {
   return (
-    <div className="container mx-auto px-4 py-12">
-      <h1 className="text-3xl font-bold mb-8">Security Features</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
       <SecurityFeatures />
     </div>
   );
